f5e1047865e06f21592735daf89aabd6fe025c96
Andrea Coravos & Tim Cannady <pair+acoravos.timcannady@devbootcamp.com>
2015-07-29 16:37:26 -0700

    add routes for user, categories, and items (via Jared)

--- favorite_things/config/routes.rb
+++ favorite_things/config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  get 'sessions_controller/create'
 
-  get 'sessions_controller/destroy'
+  resources :users do
+    resources :items, only: [:index]
+    get '/users/:user_id/categories/:id', to: 'users#items_by_category', as: 'items_by_category'
+    # resources :categories, only: [:show], controller: 'users'
+  end
+
+  resources :categories do
+    resources :items, only: [:index]
+  end
+
+  resources :items
 
 end
