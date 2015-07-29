d87296b5673a90d1d891cd94d4e7f34fcd71b508
Andrea Coravos & Tim Cannady <pair+acoravos.timcannady@devbootcamp.com>
2015-07-29 16:50:49 -0700

    Add bcrypt to gemfile

--- favorite_things/app/controllers/users_controller.rb
+++ favorite_things/app/controllers/users_controller.rb
@@ -1,3 +1,10 @@
 class UsersController < ApplicationController
 
+  def home
+    if current_user = nil
+      redirect_to login_path
+    else
+      redirect_to user_show_path
+  end
+
 end
--- favorite_things/config/routes.rb
+++ favorite_things/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get '/login_signup', to: "sessions#login"
+  post '/login', to: "sessions#create"
+  get '/logout', to: "sessions#destroy"
+
   resources :users do
     resources :items, only: [:index]
     get '/users/:user_id/categories/:id', to: 'users#items_by_category', as: 'items_by_category'
@@ -11,5 +15,7 @@ Rails.application.routes.draw do
   end
 
   resources :items
+
+  root "users#home"
 
 end
